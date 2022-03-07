3ab1b6f4afab22250227268ef5d7d4bdfa7a41f9
Frank Cipolla <frankc@cscit.net>
2022-03-07 14:13:16 -0500

    Initial Commit

---
+++ Blocks/BlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlocksApp.swift
+//  Blocks
+//
+//  Created by Frank Cipolla on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blocks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Blocks
+//
+//  Created by Frank Cipolla on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("News")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
