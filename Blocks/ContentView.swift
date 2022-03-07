51fc5dbfcf2e03eed83b322439356af6685ed302
Frank Cipolla <frankc@cscit.net>
2022-03-07 16:20:52 -0500

    Added tabs and a rREADME file

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("News")
-            .padding()
+        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+            Text("Tab Content 1").tabItem { Text("News") }.tag(1)
+            Text("Tab Content 2").tabItem { Text("Products") }.tag(2)
+            Text("Tab Content 3").tabItem { Text("Chat") }.tag(3)
+
+        }
+       
     }
 }
 
