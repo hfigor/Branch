a20d4c3c3eeaf5ac8f9f6017d3ba9c3bbe5ea5dc
Frank Cipolla <frankc@cscit.net>
2022-03-08 15:09:11 -0500

    Added a list to the news tab

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -11,26 +11,31 @@ struct ContentView: View {
     var body: some View {
         TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
             NavigationView {
-                Text("Text")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .navigationTitle("News")
+                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Text")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
+                    
+                }
+                .navigationTitle("News")
             }
             .tabItem { Text("News") }.tag(1)
             NavigationView {
                 Text("Tab Content 2")
-            .navigationTitle("Products")
+                    .navigationTitle("Products")
             }
             .tabItem { Text("Products") }.tag(2)
             NavigationView {
                 Text("Tab Content 3")
-            .navigationTitle("Chat")
+                    .navigationTitle("Chat")
             }
             .tabItem { Text("Chat") }.tag(3)
-
+            
         }
         .accentColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-       
+        
     }
 }
 
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Referencing tutorioals:
 https://medium.nextlevelswift.com/build-an-app-like-lego-with-swiftui-tutorial-2-2d01177e35c
 https://medium.nextlevelswift.com Tom Brodhurst-Hill https://barefeettom.medium.com
+https://medium.nextlevelswift.com/build-an-app-like-lego-with-swiftui-tutorial-5-35859ca30908
 *************
 
 Testing Notes:
