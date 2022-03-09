7466f553f7b4ca4a7aef34545540ffff300e6022
Frank Cipolla <frankc@cscit.net>
2022-03-09 13:31:47 -0500

    created Article Model and mock instances

---
+++ Blocks/Model/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  Blocks
+//
+//  Created by Frank Cipolla on 3/9/22.
+//
+
+import Foundation
+
+struct Article {
+    let title: String
+    let date: Date
+    let detail: String
+    let smallImageName: String
+    let largeImageName: String
+}
