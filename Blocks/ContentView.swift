912d7688c10f3da21c45db7f8af8a62b8599ca4f
Frank Cipolla <frankc@cscit.net>
2022-03-09 10:53:43 -0500

    NewsCell added HStack and Images

--- Blocks/ContentView.swift
+++ Blocks/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         NewsCell()
            // .fixedSize()
-            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/))
+            .previewLayout(.sizeThatFits)
             //.environment(\.sizeCategory, .medium)
             
         ContentView()
@@ -55,10 +55,25 @@ struct ContentView_Previews: PreviewProvider {
 struct NewsCell: View {
     var body: some View {
         VStack(alignment: .leading) {
-                Text("Text")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                Text(/*@START_MENU_TOKEN@*/"Tertiary Text"/*@END_MENU_TOKEN@*/)
+            HStack {
+                Image(systemName:"photo")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 44.0, height: 44.0)
+                    
+                VStack(alignment: .leading) {
+                        Text("Text")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        Text(/*@START_MENU_TOKEN@*/"Tertiary Text"/*@END_MENU_TOKEN@*/)
+                }
+            }
+            Text("Detailed Text")
+            Image(systemName:"photo")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .frame(maxHeight: 240.0)
+                .clipped()
         }
     }
 }
